a280afa68a5591621a4f8efab5fc517dcc7af1fc
sguillin-cell <sguillin@bu.edu>
2026-02-10 21:46:02 -0500

    Solved Part 1

--- homework_3_guessing_game/src/part1.rs
+++ homework_3_guessing_game/src/part1.rs
@@ -7,6 +7,12 @@ pub struct Part1 {}
 impl Strategy for Part1 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
         // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+        for number in min..=max {
+        let guess = number;
+        if player.ask_if_equal(guess) {
+            return guess;
+            }
+        }
+        min
     }
 }
