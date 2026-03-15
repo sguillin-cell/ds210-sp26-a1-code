595291e1863831e2aba2232bd344ac8cae1741aa
sguillin-cell <sguillin@bu.edu>
2026-03-14 21:03:20 -0600

    Finished v1

--- project_2_chatbot/basic_chatbot/src/solution/v1.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v1.rs
@@ -16,12 +16,10 @@ impl ChatbotV1 {
         let mut chat_session: Chat<Llama> = self.model
             .chat()
             .with_system_prompt("The assistant will act like a pirate");
-
-        // You need to add your code here
-        // You must find a way to add the given message to the chat_session!
-        // consider https://docs.rs/kalosm/0.4.0/kalosm/language/struct.Chat.html#method.add_message
-        // Hint: make sure you transform/extract the response message as a **String**.
-
-        return String::from("Hello, I am not a bot (yet)!");
+        let response = chat_session.add_message(message).await;
+        match response {
+        Ok(value) => value,
+        Err(_) => String::from("Error"),
+    }
     }
 }
