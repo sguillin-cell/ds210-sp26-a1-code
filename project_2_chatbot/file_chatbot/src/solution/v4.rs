e666d2010bfb17618cf8c05dcc745cc69c0e374d
sguillin-cell <sguillin@bu.edu>
2026-03-22 14:53:04 -0400

    More changes

--- project_2_chatbot/file_chatbot/src/solution/v4.rs
+++ project_2_chatbot/file_chatbot/src/solution/v4.rs
@@ -1,5 +1,5 @@
 use kalosm::language::*;
-use crate::solution::file_library::{self, load_chat_session_from_file};
+use crate::solution::file_library::{self, load_chat_session_from_file,};
 
 pub struct ChatbotV4 {
     model: Llama,
@@ -25,12 +25,13 @@ impl ChatbotV4 {
             chat_session = chat_session.with_session(session);
         }
             None => {
-            panic!("There is no file with a session");
         }
-    }
+    } 
         let response = chat_session.add_message(message).await;
         match response {
-            Ok(reply) => reply,
+            Ok(reply) =>  {
+                reply
+            }
             Err(_) => "Failed to respond, sorry!".to_string(),
         }
     }
