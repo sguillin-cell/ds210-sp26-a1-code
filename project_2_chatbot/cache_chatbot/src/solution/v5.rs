0ffcd02cc620099b26c44bc168f86dd99f7b86ad
Pria Bobby <priab@bu.edu>
2026-03-23 14:45:32 -0400

    v5 chat_with_user

--- project_2_chatbot/cache_chatbot/src/solution/v5.rs
+++ project_2_chatbot/cache_chatbot/src/solution/v5.rs
@@ -20,18 +20,36 @@ impl ChatbotV5 {
         let filename = &format!("{}.txt", username);
         let cached_chat = self.cache.get_chat(&username);
 
-        match cached_chat {
+        let mut chat = match cached_chat {
             None => {
                 println!("chat_with_user: {username} is not in the cache!");
-                // The cache does not have the chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
+                match file_library::load_chat_session_from_file(&filename) {
+                    Some(session) => {
+                        self.model
+                        .chat()
+                        .with_system_prompt("The assistent will act like a pirate")
+                        .with_session(session)
+                    }
+                    None => {
+                        self.model
+                        .chat()
+                        .with_system_prompt("The assistent will act like a pirate")
+                    }
+                }
             }
             Some(chat_session) => {
-                println!("chat_with_user: {username} is in the cache! Nice!");
-                // The cache has this chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
-
+                println!("chat_with_user: {username} is in the cache! Nice!")
+                chat_session.clone()
             }
+        };
+
+        let response = chat.add_message(message).await;
+        let session = chat.session().unwrap();
+        file_library::save_chat_session_to_file(&filename, &session);
+        self.cache.insert_chat(username.clone(), chat);
+        match response {
+            Ok(reply) => reply.to_string(),
+            Err(_) => "Error".to_string(),
         }
     }
 
