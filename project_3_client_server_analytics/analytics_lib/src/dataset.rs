ddb8d3050ae2cd483f8445586ef76ebdf594676f
sguillin-cell <sguillin@bu.edu>
2026-04-01 17:19:47 -0400

    Finished slow

--- project_3_client_server_analytics/analytics_lib/src/dataset.rs
+++ project_3_client_server_analytics/analytics_lib/src/dataset.rs
@@ -1,12 +1,14 @@
 use std::fmt::{Debug, Display};
-
-#[derive(Clone, PartialEq, Eq, Debug)]
+use serde::{Deserialize, Serialize};
+#[derive(Clone, PartialEq, Eq, Debug, Serialize, Deserialize)]
 pub enum ColumnType {
     String,
     Integer,
 }
 
 #[derive(Clone, PartialEq, Hash, Eq, Debug, PartialOrd, Ord)]
+
+#[derive(Serialize, Deserialize)]
 pub enum Value {
     String(String),
     Integer(i32),
@@ -21,6 +23,8 @@ impl Value {
 }
 
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord)]
+
+#[derive(Serialize, Deserialize)]
 pub struct Row {
     values: Vec<Value>,
 }
@@ -39,6 +43,7 @@ impl Row {
     }
 }
 
+#[derive(Clone,Serialize, Deserialize)]  
 pub struct Dataset {
     columns: Vec<(String, ColumnType)>,
     rows: Vec<Row>, 
--- project_3_client_server_analytics/interface/src/lib.rs
+++ project_3_client_server_analytics/interface/src/lib.rs
@@ -4,6 +4,7 @@ use analytics_lib::{dataset::Dataset, query::Query};
 #[tarpc::service]
 pub trait RPCInterface {
     async fn hello() -> String;
-    // async fn slow_rpc() -> Dataset;
+    
+    async fn slow_rpc() -> Dataset;
     // async fn fast_rpc(query: Query) -> Dataset;
 }
